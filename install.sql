9100ad6daa54021b0292c35f8b0143ba8c1977dd
Thomas Elvin <info@datalaere.dk>
2021-09-22 13:33:17 +0200

    Update install.sql

--- install.sql
+++ install.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Vært: 127.0.0.1
--- Genereringstid: 21. 09 2021 kl. 14:49:47
+-- Genereringstid: 22. 09 2021 kl. 13:32:44
 -- Serverversion: 10.4.21-MariaDB
 -- PHP-version: 8.0.10
 
@@ -44,10 +44,9 @@ CREATE TABLE `whish_gifts` (
 --
 
 INSERT INTO `whish_gifts` (`gift_id`, `gift_image`, `gift_name`, `gift_qty`, `gift_reservations`, `gift_price`, `gift_note`, `gift_link`, `gift_list`) VALUES
-(1, 'https://y-design.dk/wp-content/uploads/2020/06/20-RC912-royal-copenhagen-kande-med-laag-stemning-600x600.jpg', 'Royal copenhagen middagstallerken nr. 19', 1, 0, 250, NULL, NULL, 1),
-(2, '', 'Medisterpølse', 2, 0, 300, '', '', 3),
-(3, '', 'Kande', 2, 0, 200, '', '', 1),
-(4, '', 'Kande', 2, 0, 200, '', '', 1);
+(1, 'https://y-design.dk/wp-content/uploads/2020/06/20-RC912-royal-copenhagen-kande-med-laag-stemning-600x600.jpg', 'Royal copenhagen middagstallerken', 3, 0, 250, 'KØB ALLE', '', 1),
+(9, '', 'Kaffekande', 6, 0, 200, '', '', 2),
+(10, '', 'Briller', 2, 0, 500, '', '', 2);
 
 -- --------------------------------------------------------
 
@@ -62,17 +61,17 @@ CREATE TABLE `whish_lists` (
   `list_subtitle` text DEFAULT NULL,
   `list_link` varchar(255) DEFAULT NULL,
   `list_user` bigint(20) DEFAULT NULL,
-  `list_code` varchar(255) NOT NULL
+  `list_code` varchar(255) NOT NULL,
+  `list_active` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Data dump for tabellen `whish_lists`
 --
 
-INSERT INTO `whish_lists` (`list_id`, `list_date`, `list_title`, `list_subtitle`, `list_link`, `list_user`, `list_code`) VALUES
-(1, '2021-09-21 12:00:42', 'Min fødselsdag i morgen', '2022', NULL, 1, '12345'),
-(2, '2021-09-21 12:00:42', 'Mit bryllup', '2022', NULL, NULL, '54321'),
-(3, '2022-05-19 12:31:35', 'Test1234', 'test', NULL, 1, '192133');
+INSERT INTO `whish_lists` (`list_id`, `list_date`, `list_title`, `list_subtitle`, `list_link`, `list_user`, `list_code`, `list_active`) VALUES
+(1, '2021-09-21 12:00:42', 'Min fødselsdag i morgen', '2022', NULL, 1, '12345', 1),
+(2, '2022-08-13 00:05:36', 'Bryllup', 'Caroline & Thomas', NULL, NULL, '54321', 1);
 
 -- --------------------------------------------------------
 
@@ -85,7 +84,7 @@ CREATE TABLE `whish_sessions` (
   `session_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `session_list` bigint(20) NOT NULL,
   `session_hash` varchar(255) DEFAULT NULL,
-  `session_products` text DEFAULT NULL
+  `session_gifts` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -107,7 +106,7 @@ CREATE TABLE `whish_users` (
 --
 
 INSERT INTO `whish_users` (`user_id`, `user_name`, `user_pass`, `user_email`, `user_reset`) VALUES
-(1, 'john', 'fb469d7ef430b0baf0cab6c436e70375', 'john@example.com', '');
+(1, 'john', '1a1dc91c907325c69271ddf0c944bc72', 'john@example.com', '');
 
 --
 -- Begrænsninger for dumpede tabeller
@@ -145,7 +144,7 @@ ALTER TABLE `whish_users`
 -- Tilføj AUTO_INCREMENT i tabel `whish_gifts`
 --
 ALTER TABLE `whish_gifts`
-  MODIFY `gift_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `gift_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Tilføj AUTO_INCREMENT i tabel `whish_lists`
@@ -157,7 +156,7 @@ ALTER TABLE `whish_lists`
 -- Tilføj AUTO_INCREMENT i tabel `whish_sessions`
 --
 ALTER TABLE `whish_sessions`
-  MODIFY `session_id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `session_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Tilføj AUTO_INCREMENT i tabel `whish_users`
